753b784eaf37ee128354fdf3985d38887546181a
Richard Chin <99560658+rickchin-shue@users.noreply.github.com>
2022-02-12 20:54:11 +0000

    Add files via upload

---
+++ donorfy_donations_HMRC.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 21:52:05 2022
+
+@author: Lenovo
+"""
+
+import pandas as pd
+import numpy as np
+import os
+
+os.chdir(r'C:\Users\Lenovo\Documents\Work\data')
+filename = 'DonationsGiftAid.csv'
+df = pd.read_csv(filename)
+
+colren = {'First name or initial':'firstname', 'Last name':'lastname', 'House name or number':'address1','Donation date':'date'}
+df.rename(columns=colren, inplace=True)
+df.head()
+
+df.drop(columns =[ 'Title','Aggregated donations', 'Sponsored event'], inplace=True)
+df
+x = df.sort_values('date')
+x['date2'] = pd.to_datetime( df['date'] )
+x.head()
+x = x.sort_values('date2')
+x.head()
+
+x['fullname'] = x.lastname + '.' + x.firstname
+
+
+
+
+y = x.groupby('fullname').agg({'date2':['count','min','max']})
+y.sort_values(('date2','count'), ascending=False, inplace=True)
+y.head(10)
